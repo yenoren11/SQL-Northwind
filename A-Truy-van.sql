673553b22d7d47b67b016290f067aa80b2f1135c
Hoàng Yến <163098118+yenoren11@users.noreply.github.com>
2025-12-31 10:01:59 +0700

    Add files via upload

---
+++ A-Truy-van.sql
@@ -0,0 +1,124 @@
+﻿--A. Câu Truy vấn
+--1.Lập danh sách các Customer là Sales Manager của nước Mỹ (USA) hoặc là Owner 
+--của Mexico. Danh sách gồm: Customer ID, Company Name, Contact Name, Address, 
+--City, Contact Title.
+SELECT	CustomerID, CompanyName, ContactName, Address, City, ContactTitle
+FROM	Customers
+WHERE	(ContactTitle = 'Sales Manager' AND Country = 'USA')
+OR		(ContactTitle = 'Owner' AND Country = 'Mexico')
+
+--2.Liệt kê danh sách khách hàng mà trong tên công ty có chứa một chữ “a” và một chữ “e”.
+SELECT	* 
+FROM	Customers
+WHERE	CompanyName like '%a%e%'
+
+--3.Lập danh sách các đơn hàng trong tháng 4/97 gồm các thông tin sau: Order ID, Order
+--Date, Customer, Employee, Freight, New-Freight trong đó New-Freight =110%
+--Freight.
+SELECT	OrderID, OrderDate, CompanyName, FirstName + ' ' + LastName as EmployeeName, Freight, 'New-Freight' = 1.1*O.Freight
+FROM	Customers C, Orders O, Employees E
+WHERE	C.CustomerID = O.CustomerID
+AND		O.EmployeeID = E.EmployeeID
+AND		MONTH(O.OrderDate) = 4
+AND		YEAR(O.OrderDate) = 1997
+
+--4.Lập danh sách các Order có ngày đặt hàng các năm chẳn. Danh sách gồm: Order ID,
+--Order Date, Customer, Employee trong đó Customer là Company Name của khách
+--hàng, Employee lấy Last Name
+SELECT	OrderID, OrderDate, CompanyName, LastName as EmployeeName
+FROM	Customers C, Orders O, Employees E
+WHERE	C.CustomerID = O.CustomerID
+AND		O.EmployeeID = E.EmployeeID
+AND		YEAR(O.OrderDate)%2=0
+--4.1.Lập danh sách các khách hàng có ngày đặt hàng các năm chẳn. 
+SELECT	DISTINCT C.*
+FROM	Customers C, Orders O
+WHERE	C.CustomerID = O.CustomerID
+AND		YEAR(O.OrderDate)%2=0
+--4.2.Lập danh sách các khách hàng mà mã KH đó là các mã KH có ngày đặt hàng các năm chẳn.
+SELECT	*
+FROM	Customers
+WHERE	CustomerID in (SELECT DISTINCT CustomerID 
+					   FROM Orders
+					   WHERE YEAR(OrderDate)%2=0)
+
+--5.Tính đơn giá trung bình cuả các sản phẩm có đơn giá lớn hơn 30, nhóm theo loại sản phẩm
+SELECT	AVG(UnitPrice) as 'Average Price', CategoryID
+FROM	Products
+WHERE	UnitPrice > 30
+GROUP BY CategoryID
+
+--6.Lập danh sách các loại sản phẩm có đơn giá trung bình lớn hơn 30
+SELECT	C.CategoryID, C.CategoryName, AVG(P.UnitPrice) as 'Average Price'
+FROM	Products P, Categories C
+WHERE	C.CategoryID = P.CategoryID
+GROUP BY C.CategoryID, C.CategoryName
+HAVING	AVG(P.UnitPrice) > 30
+
+--7.Người ta muốn có danh sách chi tiết các hoá đơn (Order Details) trong năm 1997.
+--Danh sách gồm các thông tin: Order ID, ProductName, Unit Price, Quantity,
+--ThanhTien, Discount, TienGiamGia, TienPhaiTra trong đó: ThanhTien = UnitPrice*Quantity, 
+--TienGiamGia = ThanhTien *Discount, TienPhaiTra = ThanhTien – TienGiamGia
+SELECT	OD.OrderID, P.ProductName, OD.UnitPrice, OD.Quantity,
+		'ThanhTien' = OD.UnitPrice * OD.Quantity,
+		'TienGiamGia' = OD.UnitPrice * OD.Quantity * Discount,
+		'TienPhaiTra' = OD.UnitPrice * OD.Quantity - OD.UnitPrice * OD.Quantity * Discount
+FROM	[Order Details] OD, Products P, Orders O
+WHERE	OD.ProductID = P.ProductID
+AND		O.OrderID = OD.OrderID 
+AND		YEAR(O.OrderDate) = 1997
+
+--8.Thiết kế query tính doanh số của từng loại sản phẩm (Category) trong năm 1996. Danh
+--sách gồm 2 cột: Category Name, Sales; trong đó SalesTotal = UnitPrice*Quantity*(1-Discount)
+SELECT	C.CategoryName,
+		ROUND (SUM(OD.UnitPrice*OD.Quantity*(1-OD.Discount)),2) as [SalesTotal] -- Round: làm tròn doanh số đến 2 chữ số thập phân.
+FROM	Categories C, Orders O, [Order Details] OD, Products P
+WHERE	C.CategoryID = P.CategoryID
+AND		P.ProductID = OD.ProductID
+AND		OD.OrderID = O.OrderID
+AND		YEAR(OrderDate) = 1996
+GROUP BY C.CategoryName
+
+--9.Thiết kế query tính tỉ lệ tiền cước mua hàng (Freight) của từng khách hàng trong năm
+--1997. Danh sách gồm các cột: Company Name (của Customer), Freight, SalesTotal = UnitPrice * Quantity*(1-Discount), 
+--Percent= Freight/SalesTotal.
+SELECT	C.CompanyName,
+		ROUND (SUM(O.Freight), 2) as [Freight],
+		ROUND (SUM(OD.UnitPrice*OD.Quantity*(1-OD.Discount)),2) as [SalesTotal],
+		ROUND (SUM(O.Freight)/SUM(OD.UnitPrice*OD.Quantity*(1-OD.Discount)),2) as [Percent]
+FROM	Customers C, Orders O, [Order Details] OD
+WHERE	C.CustomerID = O.CustomerID
+AND		OD.OrderID = O.OrderID
+AND		YEAR(OrderDate) = 1997
+GROUP BY C.CompanyName
+
+--10.Liệt kê danh sách nhân viên được thuê sau nhân viên Andrew.
+SELECT	*
+FROM	Employees
+WHERE	HireDate > (SELECT HireDate
+				    FROM Employees
+				    WHERE FirstName = 'Andrew')
+--11.Liệt kê các sản phẩm có cùng nhà cung cấp với sản phẩm Tofu.
+SELECT	*
+FROM	Products
+WHERE	SupplierID in (SELECT SupplierID
+					   FROM Products
+					   WHERE ProductName ='Tofu')
+--12.Cho biết sản phẩm được đặt nhiều nhất và đặt ít nhất trong ba tháng cuối năm 1997.
+SELECT	*
+FROM	Products
+WHERE	ProductID IN (SELECT TOP 1 ProductID
+					  FROM [Order Details] OD, Orders O
+					  WHERE MONTH(OrderDate) in (10,11,12)
+					  AND YEAR(OrderDate) = 1997
+					  GROUP BY ProductID
+					  ORDER BY SUM(Quantity) DESC)
+UNION ALL
+SELECT	*
+FROM	Products
+WHERE	ProductID IN (SELECT TOP 1 ProductID
+					  FROM [Order Details] OD, Orders O
+					  WHERE MONTH(OrderDate) in (10,11,12)
+					  AND YEAR(OrderDate) = 1997
+					  GROUP BY ProductID
+					  ORDER BY SUM(Quantity) ASC)
---
+++ B-Ma-kich-ban.sql
@@ -0,0 +1,157 @@
+﻿--B.Mã kịch bản
+--13.Tạo danh sách các Product thuộc một loại nào đó khi nhập Category ID từ bàn phím.
+--Danh sách gồm: Product Name, Unit Price, Supplier
+CREATE PROC SP_GetProductbyCateID @CateID int
+AS
+BEGIN
+	SELECT	P.ProductName, P.UnitPrice, S.CompanyName as SupplierName
+	FROM	Products P, Suppliers S
+	WHERE	P.SupplierID = S.SupplierID
+	AND		P.CategoryID = @CateID
+END
+GO
+
+EXEC SP_GetProductbyCateID @CateID = 2
+DROP PROC SP_GetProductbyCateName
+
+--Trường hợp nhập tên loại hàng thay vì ID
+CREATE PROC SP_GetProductbyCateName @CateName NVARCHAR(100)
+AS
+BEGIN
+	SELECT	P.ProductName, P.UnitPrice, S.CompanyName as SupplierName
+	FROM	Products P, Suppliers S, Categories C
+	WHERE	P.SupplierID = S.SupplierID
+	AND		C.CategoryID = P.CategoryID 
+	AND		C.CategoryName = @CateName	
+END
+GO
+
+EXEC SP_GetProductbyCateName @CateName = 'Condiments'
+DROP PROC SP_GetProductbyCateName
+
+--14.Lập danh sách các Order có Order Date trong 1 khoảng thời gian nào đó. Tham số
+--nhập: “From date:” và “To:”. Danh sách gồm: Order ID, Customer, ShipCountry,
+--OrderDate
+CREATE PROC SP_ListOrderbyDate @StartDate DATE, @EndDate DATE
+AS
+BEGIN
+	SELECT	O.OrderID, C.CompanyName AS Customer, O.ShipCountry, O.OrderDate
+	FROM	Orders O, Customers C
+	WHERE	O.CustomerID = C.CustomerID
+	AND		OrderDate BETWEEN @StartDate AND @EndDate
+END
+GO
+
+SET DATEFORMAT DMY
+EXEC SP_ListOrderbyDate @StartDate = '30/3/1997', @EndDate = '30/6/1997'
+DROP PROC SP_ListOrderbyDate
+
+--15.Viết khối lệnh hiển thị số đơn hàng từng nhân viên đã lập trong năm 1997, nếu số lượng
+--đơn ít hơn 20 thì hiện thông báo “số lượng đơn quá ít”
+SELECT	E.EmployeeID, 
+		E.FirstName + ' ' + E.LastName AS EmployeeName,
+		COUNT(O.OrderID) AS OrderCount,
+		CASE
+			WHEN COUNT(O.OrderID) < 20 THEN N'Số lượng đơn quá ít'
+			ELSE ''
+		END AS Note
+FROM	Orders O, Employees E
+WHERE	O.EmployeeID = E.EmployeeID
+AND		YEAR(O.OrderDate) = 1997
+GROUP BY E.EmployeeID, E.FirstName, E.LastName
+ORDER BY 2 --Sắp xếp theo cột thứ 2 trong bảng
+
+--Trường hợp nếu làm theo các năm động
+CREATE PROC SP_CountOrderByEmployee @Year int
+AS
+BEGIN
+	SELECT	E.EmployeeID, 
+			E.FirstName + ' ' + E.LastName AS EmployeeName,
+			COUNT(O.OrderID) AS OrderCount,
+			CASE
+				WHEN COUNT(O.OrderID) < 20 THEN N'Số lượng đơn quá ít'
+				ELSE ''
+			END AS Note
+	FROM	Orders O, Employees E
+	WHERE	O.EmployeeID = E.EmployeeID
+	AND		YEAR(O.OrderDate) = @Year
+	GROUP BY E.EmployeeID, E.FirstName, E.LastName
+END
+GO
+
+EXEC SP_CountOrderByEmployee @Year = 1997
+DROP PROC SP_CountOrderByEmployee
+
+--16.Người ta muốn biết trong một ngày nào đó có số lượng đơn đặt hàng theo từng khách
+--hàng cần phải hoàn tất hay không? (theo Required Date).
+CREATE PROC SP_OrderRequiredDate @Date DATE
+AS
+BEGIN
+	SELECT	C.CustomerID, C.CompanyName as CustomerName,
+			COUNT(O.OrderID) AS CountOfOrderID
+	FROM	Orders O, Customers C
+	WHERE	O.CustomerID = C.CustomerID
+	AND		O.RequiredDate = @Date
+	GROUP BY C.CustomerID, C.CompanyName
+END
+GO
+
+EXEC SP_OrderRequiredDate @Date = '9/4/1997'
+DROP PROC SP_OrderRequiredDate
+
+--17.Thông thường các khách hàng muốn biết thông tin về đơn hàng của họ đã đặt hàng
+--vào một ngày nào đó. (Khách hàng sẽ báo tên công ty và ngày đặt hàng). Thông
+--tin gồm tất cả các cột của table Order. Anh chị hãy thiết kế query để thực hiện điều
+--này.
+CREATE PROC SP_OrderByNameDate @Name NVARCHAR(40), @Date DATE
+AS
+BEGIN
+	SELECT	* 
+	FROM	Customers C, Orders O
+	WHERE	O.CustomerID = C.CustomerID
+	AND		C.CompanyName = @Name
+	AND		CAST(O.OrderDate AS DATE) = @Date
+END
+GO
+
+EXEC SP_OrderByNameDate @Name = 'Eastern Connection', @Date = '1/1/1997'
+DROP PROC SP_OrderByNameDate
+
+--18.Tương tự nhưng năm được nhập từ bàn phím; trong đó nếu không nhập năm mà
+--chỉ Enter thì sẽ lấy năm hiện tại để tính.
+CREATE PROC SP_OrderByCustomerYear @Name NVARCHAR(40), @Year INT = NULL
+AS
+BEGIN
+	IF @Year IS NULL
+		SET @Year = YEAR(GETDATE())
+	SELECT	* 
+	FROM	Customers C, Orders O
+	WHERE	O.CustomerID = C.CustomerID
+	AND		C.CompanyName = @Name
+	AND		YEAR(O.OrderDate) = @Year
+END
+GO
+
+EXEC SP_OrderByCustomerYear @Name = 'Eastern Connection'
+DROP PROC SP_OrderByCustomerYear
+
+--19.Người ta muốn biết trong một ngày nào đó (nếu chỉ Enter là ngày hiện tại) tổng số
+--đơn đặt hàng và doanh số cuả các đơn hàng đó là bao nhiêu. Thí dụ nhập 7 thang
+--4 nam 1998 thì kết quả sẽ là:
+CREATE PROC SP_SumOfOrdersandRevenue @Date DATE = NULL
+AS
+BEGIN
+	IF @Date IS NULL
+		SET @Date = CAST(GETDATE() AS DATE)
+	SELECT	O.OrderDate,
+			COUNT(DISTINCT O.OrderID) AS TotalOrders,
+			SUM(OD.UnitPrice * OD.Quantity * (1 - OD.Discount)) AS TotalRevenue
+	FROM	Orders O, [Order Details] OD
+	WHERE	O.OrderID = OD.OrderID
+	AND		O.OrderDate = @Date
+	GROUP BY O.OrderDate
+END
+GO
+
+EXEC SP_SumOfOrdersandRevenue @Date = '1/5/1997'
+DROP PROC SP_SumOfOrdersandRevenue
